e6570198c23aa9d51ff30af87600a72217c30d4d
CEO-scripts <charlesokorie594@gmail.com>
2024-11-01 16:32:34 +0100

    Dog Years

---
+++ app.js
@@ -0,0 +1,25 @@
+//Creating a Variable called myAge
+const myAge = 20
+
+//Creating a variable called earlyYears
+let earlyYears = 2
+
+//Multiplying earlyYears by 10.5
+earlyYears *= 10.5
+
+//Creating a variable called laterYears
+let laterYears = myAge - 2
+
+//Calculating the Dog Years
+laterYears *= 4
+
+//My Age in Dog Years
+const myAgeInDogYears = earlyYears + laterYears
+
+//Creating a variable myName
+let myName = 'Charles' 
+myName = myName.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
+
+
